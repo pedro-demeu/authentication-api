0073e72582dd560b5060b021f5c60ece6287d198
pdemeu <pdemeu@outlook.com>
2023-09-02 16:44:18 -0300

    Config: Set AppError to handle with any error in application

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,8 @@
-import express from 'express'
-import cors from 'cors'
-import routes from './routes'
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import routes from './routes';
+import AppError from '@shared/errors/AppError';
+
 const app = express();
 const PORT = 3333;
 
@@ -8,6 +10,20 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT} 🥇`)
-})
+  console.log(`Server is running on port ${PORT} 🥇`);
+});
