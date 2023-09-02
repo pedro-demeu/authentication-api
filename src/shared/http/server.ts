413b47fe9148088bfcb1797a1cd35f20d28b9582
pdemeu <pdemeu@outlook.com>
2023-09-02 16:36:58 -0300

    First Commit: Add Typescript, Express, Eslint, Prettier, EditorConfig, Architecture Dir and First Executation.

---
+++ src/shared/http/server.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+import cors from 'cors'
+import routes from './routes'
+const app = express();
+const PORT = 3333;
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT} 🥇`)
+})
