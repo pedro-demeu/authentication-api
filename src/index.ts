17a8fa0275c45e67fa4b737abe12704130dd9426
pdemeu <pdemeu@outlook.com>
2023-09-05 16:14:16 -0300

    feat: better enginieer in routes

--- src/index.ts
+++ src/index.ts
@@ -1,74 +1,26 @@
 import express from 'express';
 import { config } from 'dotenv';
-import { GetUsersController } from './controllers/get-users/get-users';
-import { MongoGetUsersRepository } from './repositories/get-users/mongo-get-users';
 import { MongoClient } from './database/mongo';
-import { MongoCreateUserRepository } from './repositories/create-user/mongo-create-user';
-import { CreateUserController } from './controllers/create-user/create-user';
-import { MongoUpdateUserRepository } from './repositories/update-user/mongo-update-user';
-import { UpdateUserController } from './controllers/update-user/update-user';
-import { MongoDeleteUserRepository } from './repositories/delete-user/mongo-delete-user';
-import { DeleteUserController } from './controllers/delete-user/delete-user';
+import { routes as UserRoutes } from './modules/user';
+import { routes as AuthRoutes } from './modules/authentication';
 
 const main = async () => {
   config();
+
   const app = express();
+
   app.use(express.json());
-  const PORT = process.env.PORT || 3333;
+
   await MongoClient.connect();
 
-  app.get('/users', async (req, res) => {
-    const mongoGetUsersRepository = new MongoGetUsersRepository();
-    const getUsersController = new GetUsersController(mongoGetUsersRepository);
-
-    const { body, statusCode } = await getUsersController.handle();
-
-    res.status(statusCode).send(body);
-  });
-
-  app.post('/users', async (req, res) => {
-    const mongoCreateUserRepository = new MongoCreateUserRepository();
-    const createUserController = new CreateUserController(
-      mongoCreateUserRepository,
-    );
-
-    const { body, statusCode } = await createUserController.handle({
-      body: req.body,
-    });
-
-    res.status(statusCode).send(body);
-  });
-
-  app.put('/users/:id', async (req, res) => {
-    const mongoUpdateUserRepository = new MongoUpdateUserRepository();
-    const updateUserController = new UpdateUserController(
-      mongoUpdateUserRepository,
-    );
-
-    const { body, statusCode } = await updateUserController.handle({
-      body: req.body,
-      params: req.params,
-    });
-
-    res.status(statusCode).send(body);
-  });
-
-  app.delete('/users/:id', async (req, res) => {
-    const mongoDeleteUserRepository = new MongoDeleteUserRepository();
-    const deleteUserController = new DeleteUserController(
-      mongoDeleteUserRepository,
-    );
-
-    const { body, statusCode } = await deleteUserController.handle({
-      params: req.params,
-    });
-
-    res.status(statusCode).send(body);
-  });
+  const PORT = process.env.PORT || 3333;
 
   app.listen(PORT, () => {
     console.log(`Server is running in http://localhost:${PORT} 🚀`);
   });
+
+  app.use('/v1/api/users', UserRoutes);
+  app.use('/v1/api/auth', AuthRoutes);
 };
 
 main();
