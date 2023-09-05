e74ea1298c73b795e1e66f4bdda1caa069425b1a
pdemeu <pdemeu@outlook.com>
2023-09-05 11:23:11 -0300

    feat: add delete user controller and route

---
+++ src/controllers/delete-user/delete-user.ts
@@ -0,0 +1,31 @@
+import { HttpRequest, HttpResponse } from '@controllers/protocols';
+import { User } from '@models/User';
+import { IDeleteUserController, IDeleteUserRepository } from './protocols';
+
+export class DeleteUserController implements IDeleteUserController {
+  constructor(private readonly deleteUserRepository: IDeleteUserRepository) {}
+  async handle(httpRequest: HttpRequest<any>): Promise<HttpResponse<User>> {
+    try {
+      const id = httpRequest?.params?.id;
+
+      if (!id) {
+        return {
+          statusCode: 400,
+          body: 'id_not_found',
+        };
+      }
+
+      const user = await this.deleteUserRepository.deleteUser(id);
+
+      return {
+        statusCode: 200,
+        body: user,
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: 'something_went_wrong',
+      };
+    }
+  }
+}
--- src/controllers/delete-user/protocols.ts
+++ src/controllers/delete-user/protocols.ts
@@ -1,5 +1,10 @@
+import { HttpRequest, HttpResponse } from '@controllers/protocols';
 import { User } from '@models/User';
 
 export interface IDeleteUserRepository {
   deleteUser(id: string): Promise<User>;
+}
+
+export interface IDeleteUserController {
+  handle(httpRequest: HttpRequest<any>): Promise<HttpResponse<User>>;
 }
